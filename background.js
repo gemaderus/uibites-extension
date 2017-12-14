f65623858925f372a20b7a2400005f9a76e6e965
gemaderus <hi@gemaderus.com>
2017-12-14 09:54:28 +0100

    Finished crop image and styles.

--- background.js
+++ background.js
@@ -1,10 +1,64 @@
-// Called when the user clicks on the browser action.
+//Crop the photo
+
+const cropData = (str, coords, callback) => {
+  var img = new Image();
+
+  img.onload = () => {
+    var canvas = document.createElement('canvas');
+    canvas.width = coords.w;
+    canvas.height = coords.h;
+
+    var ctx = canvas.getContext('2d');
+
+    ctx.drawImage(img, coords.x, coords.y, coords.w, coords.h, 0, 0, coords.w, coords.h);
+
+    callback({
+      dataUri: canvas.toDataURL()
+    });
+  };
+
+  img.src = str;
+
+  document.body.appendChild(img);
+}
+
+//captureVisibleTab api de chrome captura la región visible de la tab (de la pestaña en la que esté)
+//Cropdata pilla la imagen que me devuelve el api la cargo en un canvas
+//en función de las coordenadas que le ha mandado el front hace el recorte y
+//devuelve la imagen en base64, se la mandamos al content.js con el mensaje.
+//background tiene los permisos para interactuar con el navegador por medio de la api
+
+const capture = (coords) => {
+  chrome.tabs.captureVisibleTab(null, {format: "png"}, (data) => {
+    cropData(data, coords, (data) => {
+      chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        const activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {
+          'message': 'capture_screenshot',
+          'dataUri': data.dataUri
+        });
+      });
+    });
+  });
+}
+
+// 1º Called when the user clicks on the browser action.
+//Manda un mensaje a content que es el clicked_browser_action
 chrome.browserAction.onClicked.addListener((tab) => {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     const activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {
-      'message': 'clicked_browser_action'
+      'message': 'clicked_browser_action',
+      'url': activeTab.url
     });
   });
+});
+
+//Escuchamos los eventos, en este caso coords que es el que nos envía el front
+chrome.extension.onMessage.addListener((request) => {
+  //Si la respuesta es coords, hacemos la captura
+  if (request.type == "coords") {
+    capture(request.coords);
+  }
 });
--- content.js
+++ content.js
@@ -1,10 +1,19 @@
 let status = {
   show: false,
-  token: null
+  token: null,
+  blob: null,
+  url: null
 }
 
 const ID = 'ui-bites-extension';
+const SELECTION = 'ui-bites-extension-selection';
 const CSS = 'ui-bites-extension-css';
+const IMAGE = 'ui-bites-extension-preview';
+const LOGOUT = 'ui-bites-extension-logout';
+const BASE_URL = 'http://localhost:3000';
+
+let selectionElement;
+let startPos;
 
 const hasToken = () => {
   return status.token != null;
@@ -19,12 +28,121 @@ const closeExtension = () => {
   $(el).remove();
 }
 
+//cambiamos el cursor y empezamos a escuchar cuando el usuario pincha en el documento
+const startScreenshotMode = () => {
+  console.log('start screenshot');
+  //change cursor
+  document.body.style.cursor = 'crosshair';
+  $(document).on('mousedown', mouseDown);
+}
+
+const stopScreenshotMode = () => {
+  console.log('stop screenshot');
+  //change cursor
+  document.body.style.cursor = 'default';
+  $(document).off('mousedown', mouseDown);
+}
+
+// Quitamos la cruz del cursor y dejamos de escuchar el mouseDown
+// y pasamos a background js las coordenadas
+const endScreenshot = (coords) => {
+  stopScreenshotMode();
+  chrome.runtime.sendMessage({type: 'coords', coords: coords});
+}
+
+//nos guardamos donde el usario ha pinchado y esta es la posición inicial
+const mouseDown = (e) => {
+  e.preventDefault();
+
+  startPos = {
+    x: e.pageX,
+    y: e.pageY
+  };
+
+  //Ayuda visual para saber el área que estas seleccionando
+  selectionElement = $(`<div id="${SELECTION}"></div>`);
+  selectionElement.appendTo('body');
+  selectionElement.css({
+    background: 'blue',
+    opacity: 0.1,
+    position: 'absolute',
+    left: e.pageX,
+    top: e.pageY,
+    width: 0,
+    height: 0,
+    zIndex: 1000000000
+  });
+
+  //Escuchamos cada vez que se mueve el ratón
+  $(document).on('mousemove', mouseMove);
+  $(document).on('mouseup', mouseUp);
+}
+
+//Cambiarle el tamaño a ese cuadrado
+const mouseMove = (e) => {
+  e.preventDefault();
+
+  selectionElement.css({
+    width: e.pageX - startPos.x,
+    height: e.pageY - startPos.y
+  });
+}
+
+//Te dice cuando has terminado de arrastrar.
+const mouseUp = (e) => {
+  e.preventDefault();
+
+  $(document).off('mousemove', mouseMove);
+  $(document).off('mouseup', mouseUp);
+
+  //Eliminamos el cuadro de la selección
+  selectionElement.remove();
+
+  //Cojo las coordenadas y se la paso a la función endScreenshot
+  setTimeout(() => {
+    var coords = {
+      w: e.pageX - startPos.x,
+      h: e.pageY - startPos.y,
+      x: startPos.x,
+      y: startPos.y
+    };
+
+    //Esta se la transmite de nuevo a background js
+    endScreenshot(coords);
+  }, 50);
+
+  return false;
+}
+
+const dataURItoBlob = (dataURI) => {
+  // convert base64/URLEncoded data component to raw binary data held in a string
+  var byteString;
+  if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+    byteString = atob(dataURI.split(',')[1]);
+  } else {
+    byteString = unescape(dataURI.split(',')[1]);
+  }
+
+  // separate out the mime component
+  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+  // write the bytes of the string to a typed array
+  var ia = new Uint8Array(byteString.length);
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
+  return new Blob([ia], {type:mimeString});
+}
+
+//Crea un elemento y lo añade al body y lo usamos de contenedor para los html estaticos.
 const openExtension = () => {
   const el = `<div id="${ID}" class="ui-bites-extension-container"></div>`;
   $(el).appendTo('body');
-
+  //Si hay token, mostramos el formulario
   if (hasToken()) {
     showSaveForm();
+    startScreenshotMode();
   } else {
     showLogin();
   }
@@ -37,17 +155,17 @@ const showLogin = () => {
   $.get(chrome.extension.getURL('html/login.html'), (data) => {
     $($.parseHTML(data)).appendTo(el);
 
-    $('.js-ui-bites-extension-form').on('submit', e => {
+    $('#ui-bites-extension-login').on('click', e => {
       e.preventDefault();
 
       let form = {
-        email: $('.js-ui-bites-extension-email').val(),
-        password: $('.js-ui-bites-extension-password').val()
+        email: $('#ui-bites-extension-email').val(),
+        password: $('#ui-bites-extension-password').val()
       }
 
       $.ajax({
         type: 'POST',
-        url: 'http://localhost:3000/api/auth/login',
+        url: `${BASE_URL}/api/auth/login`,
         data: JSON.stringify(form),
         contentType: "application/json; charset=utf-8",
         dataType: 'json',
@@ -55,6 +173,7 @@ const showLogin = () => {
         success: (data) => {
           chrome.storage.sync.set({'auth_token': data.token});
           status.token = data.token;
+          showSaveForm();
         },
         error: (xhr, status, error) => {
           console.error(error);
@@ -64,15 +183,63 @@ const showLogin = () => {
   });
 }
 
+//Borramos el contenido del contenedor(sombra) y cargamos el html que tenemos estático
+//y das eventos a los botones.
 const showSaveForm = () => {
   const el = `#${ID}`;
   $(el).empty();
 
   $.get(chrome.extension.getURL('html/card.html'), (data) => {
     $($.parseHTML(data)).appendTo(el);
+
+    $('#ui-bites-extension-logout').on('click', (e) => {
+      e.preventDefault();
+      chrome.storage.sync.remove(['auth_token'], (items) => {
+        status.token = null;
+        showLogin();
+      });
+
+    });
+
+    $('#ui-bites-extension-submit').on('click', (e) => {
+      e.preventDefault();
+      saveForm();
+    });
   });
 }
 
+const saveForm = () => {
+
+  //generamos un form data para enviarlos por Ajax,
+  //multipart: tiene datos y ficheros
+  var multipart = new FormData();
+
+  multipart.append('title', $('#ui-bites-extension-title').val());
+  multipart.append('description', $('#ui-bites-extension-description').val());
+  multipart.append('url', status.url);
+  multipart.append('tags', $('#ui-bites-extension-tags').val());
+  multipart.append('file', status.blob, `${Date.now()}.png`);
+
+  $.ajax({
+    type : 'POST',
+    url : `${BASE_URL}/api/dashboard/cards`,
+    data : multipart,
+    processData: false,
+    contentType: false,
+    cache: false,
+    headers: {
+      'Authorization': status.token
+    },
+    success : (data) => {
+      closeExtension();
+    },
+    error : (xhr, status, error) => {
+      console.error(error);
+    }
+  });
+}
+
+//esto lo injectamos en la página para darle estilos al forms
 const injectCSS = (css, id) => {
   const path = chrome.extension.getURL(css);
   if ($(`#${id}`).length === 0) {
@@ -85,12 +252,24 @@ const injectCSS = (css, id) => {
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === "clicked_browser_action" ) {
-      status.show = !status.show;
-      status.show ? openExtension() : closeExtension();
-    }
+  if (request.message === "clicked_browser_action" ) {
+    status.show = !status.show;
+    status.url = request.url;
+    status.show ? openExtension() : closeExtension();
   }
-);
+
+  //generamos el blob es un formato byte para la imagen
+  //y cargamos la previsualización en el form
+  //ahora el usuario tendrá que rellenar los campos
+  if (request.message === "capture_screenshot" ) {
+    status.blob = dataURItoBlob(request.dataUri);
+    const imageContainer = $(`#${IMAGE}`);
+    const image = $("<img />", {
+      "src": request.dataUri
+    });
+    imageContainer.html(image);
+  }
+});
 
 chrome.storage.sync.get(['auth_token'], function(items) {
   status.token = items.auth_token;
